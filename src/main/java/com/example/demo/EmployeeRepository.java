7b64e0511457156889d7dd873188f9fd3f10be26
arnabb38 <arnabbasak896@gmail.com>
2021-09-03 14:37:03 +0600

    updated with crud repository

--- src/main/java/com/example/demo/EmployeeRepository.java
+++ src/main/java/com/example/demo/EmployeeRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface EmployeeRepository extends PagingAndSortingRepository<Employee, Long> {
+public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
+    Page<Employee> findAll(Pageable paging);
 }
